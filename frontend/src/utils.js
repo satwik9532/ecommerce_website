42cd946b31266424fe75be6adda0c21b4ef65222
mishra <satwik@inboxkitten.com>
2021-01-24 19:58:23 +0530

    intial commit

---
+++ frontend/src/utils.js
@@ -0,0 +1,43 @@
+export const prices = [
+  {
+    name: 'Any',
+    min: 0,
+    max: 0,
+  },
+  {
+    name: `Rs 1 to Rs Rs 10`,
+    min: 1,
+    max: 10,
+  },
+  {
+    name: `Rs 10 to Rs 100`,
+    min: 10,
+    max: 100,
+  },
+  {
+    name: `Rs 100 to Rs 1000`,
+    min: 100,
+    max: 1000,
+  },
+];
+export const ratings = [
+  {
+    name: '4stars & up',
+    rating: 4,
+  },
+
+  {
+    name: '3stars & up',
+    rating: 3,
+  },
+
+  {
+    name: '2stars & up',
+    rating: 2,
+  },
+
+  {
+    name: '1stars & up',
+    rating: 1,
+  },
+];
